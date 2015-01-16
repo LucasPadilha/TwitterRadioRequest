f1b3045a7d748d3cae13d8424a9182f166475bc1
Lucas Padilha <LucasPadilha@users.noreply.github.com>
2015-01-16 20:09:30 -0200

    Create sentences.php

---
+++ include/sentences.php
@@ -0,0 +1,48 @@
+<?php
+
+$english_us = array(
+"Please play \"Ain't It Fun\" by Paramore? Thanks! #requestparamore",
+"I'd like to hear \"Ain't It Fun\" by Paramore. Please play it! #requestparamore",
+"I love \"Ain't It Fun\" by Paramore. Play it please! #requestparamore");
+
+$english = array(
+"Please play \"Ain't It Fun\" by Paramore? Thanks! #requestparamore",
+"I'd like to hear \"Ain't It Fun\" by Paramore. Please play it! #requestparamore",
+"I love \"Ain't It Fun\" by Paramore. Play it please! #requestparamore");
+
+$uk_daydreaming = array(
+"Please play \"Ain't It Fun\" by Paramore? Thanks! #requestparamore",
+"I'd like to hear \"Ain't It Fun\" by Paramore. Please play it! #requestparamore",
+"I love \"Ain't It Fun\" by Paramore. Play it please! #requestparamore");
+
+$spanish = array(
+"Por favor, podéis poner \"Ain't It Fun\" de Paramore? Gracias! #requestparamore",
+"Me gustaría escuchar \"Ain't It Fun\" de Paramore. Por favor, ponedla! #requestparamore",
+"Amo \"Ain't It Fun\" de Paramore. Ponedla, por favor! #requestparamore");
+
+$spanish1 = array(
+"Amo \"Ain't It Fun\" de Paramore. Ponla, por favor! #requestparamore",
+"Por favor, puedes poner \"Ain't It Fun\" de Paramore? Gracias! #requestparamore",
+"Me gustaría escuchar \"Ain't It Fun\" de Paramore. Por favor, ponla! #requestparamore");
+
+$spanish2 = array (
+"¿Podrían pasar \"Ain't It Fun\" de Paramore, por favor? ¡Gracias! #requestparamore",
+"Me gustaría escuchar \"Ain't It Fun\" de Paramore ¡Por favor, pasenla! #requestparamore",
+"Amo \"Ain't It Fun\" de Paramore ¡Por favor, pasenla! #requestparamore");
+
+$french = array(
+"S'il vous plaît passez \"Ain't It Fun\" de Paramore? Merci! #requestparamore",
+"J'aimerai écouter \"Ain't It Fun\" de Paramore. S'il vous plaît passez la! #requestparamore",
+"J'adore \"Ain't It Fun\" de Paramore. S'il vous  plait passez la! #requestparamore");
+
+$italian = array(
+"Potreste mandare \"Ain't It Fun\" dei Paramore? Grazie! #requestparamore",
+"Mi piacerebbe sentire \"Ain't It Fun\" dei Paramore.. potreste trasmetterla? Grazie! #requestparamore",
+"Amo \"Ain't It Fun\" dei Paramore! Per piacere trasmettetela!");
+
+$brazilian = array(
+"Por favor, toquem \"Ain't It Fun\" do Paramore? Obrigado! #requestparamore",
+"Eu queria ouvir \"Ain't It Fun\" do Paramore. Toquem, por favor! #requestparamore",
+"Amo \"Ain't It Fun\" do Paramore. Por favor, toquem ela! #requestparamore");
+
+?>
