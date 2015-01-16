ec31003e4e1d4c76ee6e4775b27f9337b8c3f0e4
Lucas Padilha <LucasPadilha@users.noreply.github.com>
2015-01-16 20:09:43 -0200

    Create stations.php

---
+++ include/stations.php
@@ -0,0 +1,233 @@
+<?php
+
+$india = array(
+"@RedFM_Mumbai",
+"@943RadioOne",
+"@Indigo919_fm");
+
+$mexico = array(
+"@ExaFM",
+"@exagdl",
+"@examonterrey",
+"@exapue",
+"@exa_queretaro",
+"@Reactor105",
+"@los40mx",
+"@CocaColaMx",
+"@alfa913fm",
+"@977_fm",
+"@Magia1017");
+
+$peru = array(
+"@Studio92",
+"@Planeta107FM",
+"@VivaFM",
+"@radiodoble9");
+
+$argentina = array(
+"@Vorterix",
+"@Los40Ar",
+"@la100fm",
+"@PopRadio1015",
+"@fmaspen",
+"@Rock_and_Pop",
+"@RadioDisneyLA");
+
+$br1 = array(
+"@atlantidabnu",
+"@atl933",
+"@atlanticosulfm",
+"@bandfmoficial",
+"@bandfmfloripa",
+"@ClubeFMBrasilia",
+"@Energia97fm",
+"@FebreMania",
+"@vox90",
+"@transamericarj",
+"@rede_atlantida",
+"@rthits",
+"@radiomixfm",
+"@RadioDisneyBRA",
+"@blink102fm",
+"@mixriofm",
+"@metropolitanafm");
+
+$us1 = array(
+"@JayMacRadio",
+"@RomeoOnSNOL",
+"@SaturdayOnline #SaturdayNightOnline",
+"@941zbq",
+"@KISSFMPhoenix",
+"@mix969",
+"@KRQQ",
+"@1027KIISFM",
+"@ampradio",
+"@Now1005FM",
+"@Wild949",
+"@alice1059",
+"@MIX100Denver",
+"@kiss957",
+"@945thebuzz");
+
+$us2 = array(
+"@MIX1051",
+"@xl1067",
+"@RadioNOW979",
+"@Q100Atlanta",
+"@1033kissfm",
+"@1035KISSFM",
+"@B96Radio",
+"@1019MIXChicago",
+"@HOT1079",
+"@KISS1075",
+"@Channel963",
+"@989RadioNow",
+"@b97",
+"@Z1043",
+"@Mix1065FM");
+
+$us3 = array(
+"@Kiss108",
+"@Mix1041",
+"@Channel955",
+"@Fresh100Detroit",
+"@1013KDWB",
+"@Y98",
+"@mix941",
+"@1043now",
+"@wzid",
+"@z100newyork",
+"@975WABB",
+"@1013kgot",
+"@999KEZ",
+"@power1057",
+"@JackFMDenver");
+
+$us4 = array(
+"@kiss959",
+"@RadioNow973",
+"@1037KISSFM",
+"@991wmyx",
+"@923NOWFM",
+"@fresh1027ny",
+"@1035KTU",
+"@Y94Official",
+"@mymix1079",
+"@979wnci",
+"@1027kj103",
+"@z100Portland",
+"@b101philly",
+"@Q102Philly",
+"@961kiss");
+
+$us5 = array(
+"@StarPittsburgh",
+"@Hot981",
+"@92PROFM",
+"@RADIOROULA",
+"@mix961sa",
+"@Hot1009",
+"@hot995",
+"@1073HITS",
+"@kissfmseattle",
+"@movin925",
+"@WVAQ",
+"@KC1013");
+
+$australia = array(
+"@nova919",
+"@nova100",
+"@nova1069",
+"@nova937",
+"@2DayFM",
+"@FoxFM",
+"@B105Brisbane",
+"@SAFMAdelaide",
+"@929Perth",
+"@NXFMNewcastle",
+"@2DayFMSydney",
+"@triplej",
+"@fresh927",
+"@TheEdgeNZ");
+
+$ecuador = array(
+"@radiodisneyEC",
+"@RadioPuntoRojo",
+"@WQRadio_EC",
+"@diamantefm",
+"@radio11q",
+"@cocacolafmec",
+"@zzradio",
+"@exafmecuador",
+"@ondaceroradioec",
+"@labrujaecuador",
+"@los40ecuador",
+"@radiocentroec",
+"@metrovisiongye",
+"@MetroStereo",
+"@OndaCeroRadioec");
+
+$spain = array(
+"@Los40_Spain",
+"@europa_fm",
+"@RAC105Oficial",
+"@radioflaixbac",
+"@m80radio_");
+
+$france = array(
+"@VirginRadiofr",
+"@RTL2officiel",
+"@ouifm",
+"@Goomradio",
+"@NRJhitmusiconly");
+
+$philippines = array(
+"@Magic899",
+"@RX931",
+"@mellow947");
+
+$uk = array(
+"@TFMradio",
+"@BBCR1",
+"@KerrangRadio",
+"@wearefreeradio",
+"@Xfm",
+"@CapitalOfficial",
+"@KissFMUK",
+"@Heartfm");
+
+$italy = array(
+"@VirginRadioIT",
+"@radiodeejay",
+"@RadioSubasio",
+"@radiodolomiti",
+"@RADIOBRUNO1",
+"@radionumberone",
+"@Radiocompany",
+"@radiokisskiss",
+"@RSD_studiodelta");
+
+$chile = array(
+"@rockandpop",
+"@RadioCarolina",
+"@40ChileOficial",
+"@RadioDisneyLA");
+
+$canada = array(
+"@FLOW935",
+"@Z1035Toronto",
+"@kiss925toronto",
+"@999virginTO");
+
+$puertorico = array(
+"@Hot102pr");
+
+$s_africa = array(
+"@947Highveld",
+"@5FM");
+
+$ireland = array(
+"@beat102103",
+"@spin1038");
+
+?>
